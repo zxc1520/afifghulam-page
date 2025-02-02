fd857676fcbdc48c4006b8040bda93e25f7b8bdb
ghulamf <afifqghulam@gmail.com>
2025-02-02 16:19:29 +0700

    add: Add Deployment Spec

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   markdown: {
     syntaxHighlight: "prism",
   },
+
+  adapter: netlify(),
 });
